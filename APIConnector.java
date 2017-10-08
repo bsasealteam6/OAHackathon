3e8faf201a5f1657b44c2cccb47e22d8149874c2
dawilco <ki4yvc@gmail.com>
2017-10-07 20:09:41 -0700

    Update documentation and comments

--- APIConnector.java
+++ APIConnector.java
@@ -7,36 +7,54 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-//import jdk.nashorn.internal.parser.JSONParser;
-import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
 import com.google.gson.*;
 
 
 public class APIConnector {
+
     private static String baseURL;
     private static String accessCode = null;
 
+    /**
+     * Create an unauthenticated endpoint
+     * @param baseURL Base URL we will call our endpoints on
+     */
     public APIConnector(String baseURL) {
         this.baseURL = baseURL;
     }
 
+    /**
+     * Create an authenticated endpoint
+     * @param baseURL Base URL we will call our endpoints on
+     * @param accessCode Access token for authenticated endpoint
+     */
     public APIConnector(String baseURL, String accessCode) {
         this.baseURL= baseURL;
         this.accessCode = accessCode;
     }
 
+    /**
+     * JSONObject returns a json object from the requested api endpoint.
+     * @param urlString the url endpoint
+     * @return JsonObject returned
+     * @requires [valid url endpoint]
+     * @ensures [json from the endpoint requested is returned]
+     */
     public static JsonObject getJSON(String urlString) {
+
         JSONObject json = null;
         JsonObject jObject = null;
+
         URL url;
         try {
+
             url = new URL(baseURL + urlString);
             System.out.println(url.toString());
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-            conn.setRequestMethod("GET");
             // set headers
+            conn.setRequestMethod("GET");
             conn.setRequestProperty("Content-Type", "text/json");
             if(accessCode != null) conn.setRequestProperty("Authorization", accessCode);
 
@@ -49,13 +67,14 @@ public class APIConnector {
                 String temp = inputLine.replace("[", "").replace("]","");
                 response.append(temp);
             }
+
             in.close();
 
             System.out.println(response.toString());
             jObject = new JsonParser().parse(response.toString()).getAsJsonObject();
 
         } catch (Exception e) {
-            System.out.println("Exception: " + e);
+            System.err.println("Exception: " + e);
         }
 
         return jObject;
